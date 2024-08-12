252106d0bab246c9fccf5f5b33ca45a631cc4932
kulkad <rdarmawan281@gmail.com>
2024-08-12 14:26:43 +0700

    halaman backup

--- src/pages/admin/backup_data/index.jsx
+++ src/pages/admin/backup_data/index.jsx
@@ -1,13 +1,51 @@
-import React from 'react'
-import { text } from 'stream/consumers'
+import AdminLayout from "../layouts";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Link from "next/link";
+import Head from "next/head";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { parseCookies } from "nookies";
+import { useRouter } from "next/router";
 
-function BackupData() {
+const BackupData = ({ isLoggedIn }) => {
+  const router = useRouter();
+
+  if (!isLoggedIn) {
+    if (typeof window !== "undefined") {
+      // Cek apakah kode sedang berjalan di sisi klien
+      router.push("/auth/login"); // Mengarahkan pengguna kembali ke halaman login
+    }
+    return <p>Loading...</p>; // or display loading indicator
+  }
   return (
-    <div>
-      <h3 className='text-center text-2xl font-semibold'>Klik Button Untuk Backup Database</h3>
-      <button></button>
-    </div>
-  )
+    <>
+      <AdminLayout>
+        <ToastContainer />
+          <div>
+            <h3 className="text-center text-xl font-semibold">Tekan Button Untuk Backup Data</h3>
+            <div className="flex justify-center py-8">
+            <button className="bg-blue-500 px-10 py-2 text-white rounded-xl" >Backup</button>
+            </div>
+              
+          </div>
+      </AdminLayout>
+    </>
+  );
+};
+
+// middleware
+export async function getServerSideProps(context) {
+  // Mendapatkan cookies dari konteks
+  const cookies = parseCookies(context);
+
+  // Mengecek apakah token JWT ada di cookies
+  const isLoggedIn = !!cookies.token;
+
+  // Mengembalikan props untuk komponen Dashboard
+  return {
+    props: { isLoggedIn },
+  };
 }
 
-export default BackupData
+export default BackupData;
