386203851954d04ad365b510e5dcc04f3b6c8fcb
kulkad <rdarmawan281@gmail.com>
2024-08-13 15:50:11 +0700

    tampilan untuk tambah data kategori klien

--- src/pages/admin/kategoriKlien/add.jsx
+++ src/pages/admin/kategoriKlien/add.jsx
@@ -46,13 +46,15 @@ export default function Add() {
   return (
     <AdminLayout>
       <div className="flex items-center justify-center p-12 ">
-        <div className="mx-auto w-full max-w-[550px] bg-white rounded-lg  lg:-mt-48">
-          <Link href={"/admin/kategoriKlien"} className="relative ml-32 lg:ml-60 ">
-            <div className="absolute flex items-center gap-2 px-8 py-2 font-semibold text-white rounded-lg cursor-pointer m text-end bg-gradient-to-r from-indigo-400 to-gray-600 lg:left-24 left-4 top-10 text-md">
-              <i className="fas fa-arrow-left"></i>
-              <span>Kembali</span>
-            </div>
-          </Link>
+        <div className="mx-auto w-full max-w-[700px] bg-white rounded-lg  lg:-mt-28">
+        <div className="relative py-2">
+            <Link href={"/admin/kategoriKlien"} className="absolute right-4 top-10">
+              <div className="flex items-center gap-2 px-8 py-2 font-semibold text-white rounded-lg cursor-pointer bg-orange-400 text-md">
+                <i className="fas fa-arrow-left"></i>
+                <span>Kembali</span>
+              </div>
+            </Link>
+          </div>
           <form className="py-6 bg-white px-9" onSubmit={handleSubmit}>
             <div className="mt-4 mb-5">
               <label
@@ -65,7 +67,7 @@ export default function Add() {
                 type="text"
                 name="nama_kategori_klien"
                 id="nama_kategori_klien"
-                className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                className="w-full rounded-md border-2 border-indigo-300 bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
                 value={formData.nama_kategori_klien}
                 onChange={handleInputChange}
                 required
@@ -73,7 +75,7 @@ export default function Add() {
             </div>
 
             <div>
-              <button className="w-full px-8 py-3 text-base font-semibold text-center text-white rounded-md outline-none hover:shadow-form bg-gradient-to-r from-indigo-400 to-gray-600 hover:bg-indigo-400 focus:bg-indigo-400">
+              <button className="w-full px-8 py-3 text-base font-semibold text-center text-white rounded-md outline-none hover:shadow-form bg-blue-400 hover:bg-indigo-600 focus:bg-indigo-400">
                 Simpan
               </button>
             </div>
