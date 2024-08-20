1526784b302a6274b7a90c041d3cbd77b486dbbb
kulkad <rdarmawan281@gmail.com>
2024-08-20 14:31:56 +0700

    update tampilan table invoice

--- src/pages/pelanggan/invoice/index.jsx
+++ src/pages/pelanggan/invoice/index.jsx
@@ -71,161 +71,61 @@ export default function Invoice() {
   return (
     <PelangganLayout>
       <Head>
-        <title>Invoice</title>
+        <title>Data Invoice</title>
       </Head>
-      <div className="flex flex-col overflow-hidden bg-white rounded-xl md:-mt-44">
-        <div className="sm:-mx-6 lg:-mx-8">
-          <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
-            <div className="invoice-container p-6 bg-white rounded-md shadow-md">
-              {/* Konten invoice */}
-              <div className="flex justify-between mb-6">
-                <div>
-                  <h1 className="text-xl font-bold mb-2">
-                    {settingData?.profil_perusahaan}
-                  </h1>
-                  <img
-                    className="w-20 h-20 mx-16 mt-4"
-                    src={settingData?.gambar_setting}
-                    alt=""
-                  />
-                </div>
-                <div className="mx-auto">
-                  <h1 className="text-xl font-bold pb-2">Invoice</h1>
-                  <div className="text-gray-600 grid grid-flow-col">
-                    <div>Referensi</div>
-                    <div className="">INV/00001</div>
-                  </div>
-                  <div className="text-gray-600 grid grid-flow-col">
-                    <div>Tanggal</div>
-                    <div className="">15/08/2024</div>
-                  </div>
-                  <div className="text-gray-600 grid grid-flow-col">
-                    <div>Tgl. Jatuh Tempo </div>
-                    <div className="">15/08/2024</div>
-                  </div>
-                </div>
-              </div>
-
-              <div className="grid grid-cols-2 pt-6 pb-12">
-                <div className="">
-                  <h3 className="font-bold">Informasi Perusahaan</h3>
-                  <hr className="border-black w-3/4 mb-4" />
-                  <div className="text-gray-600">
-                    <div>
-                      Nama Perusahaan : {settingData?.profil_perusahaan}
-                    </div>
-                    <div>Alamat Perusahaan: {settingData?.alamat}</div>
-                    <div>Telefon Perusahaan : {settingData?.telp}</div>
-                    <div>Email: Perusahaan : {settingData?.email}</div>
-                  </div>
-                </div>
-
-                <div className="">
-                  <h3 className="font-bold">Tagihan Kepada</h3>
-                  <hr className="border-black w-3/4 mb-4" />
-                  <div className="text-gray-600">
-                    <div>{customerData?.nama}</div>
-                    <div>{customerData?.alamat}</div>
-                    <div>{customerData?.no_telp}</div>
-                    <div>{customerData?.email}</div>
-                  </div>
-                </div>
-              </div>
-
-              <div className="mb-6 mx-auto">
-                <table className="min-w-full divide-y divide-gray-200">
-                  <thead className="bg-gray-700 text-white">
+      <div className="flex flex-col overflow-x-auto bg-white rounded-xl md:-mt-44">
+          <div className="sm:-mx-6 lg:-mx-8">
+            <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+              <div className="overflow-x-auto">
+                <table className="min-w-full text-sm font-light text-left">
+                  <thead className="font-medium border-b dark:border-neutral-500">
                     <tr>
-                      <th className="mx-auto py-3 text-center text-xs font-medium uppercase tracking-wider">
-                        Paket
+                      <th scope="col" className="px-12 py-4">
+                        Nama Paket
                       </th>
-                      <th className="mx-auto py-3 text-center text-xs font-medium uppercase tracking-wider">
+                      <th scope="col" className="px-12 py-4">
                         Nama Kategori
                       </th>
-                      <th className="mx-auto py-3 text-center text-xs font-medium uppercase tracking-wider">
+                      <th scope="col" className="px-12 py-4">
                         Harga
                       </th>
-                      <th className="mx-auto py-3 text-center text-xs font-medium uppercase tracking-wider">
-                        Diskon
-                      </th>
-                      <th className="mx-auto py-3 text-center text-xs font-medium uppercase tracking-wider">
-                        Pajak
-                      </th>
-                      <th className="mx-auto py-3 text-center text-xs font-medium uppercase tracking-wider">
-                        Jumlah
+                      <th scope="col" className="px-12 py-4">
+                        Action
                       </th>
                     </tr>
                   </thead>
-                  <tbody className="bg-gray-300 divide-y divide-gray-200">
-                    {cartPaketData.map((item, index) => (
+                  <tbody>
+                  {cartPaketData.map((item, index) => (
                       <tr key={index}>
-                        <td className="mx-auto text-sm py-4 text-center">
-                          {item.pakets.nama_paket}
+                        <td className="px-12 py-4 whitespace-nowrap">
+                        {item.pakets.nama_paket}
                         </td>
-                        <td className="mx-auto text-sm py-4 text-center">
-                          {item.pakets.kategoriWebsite.nama_kategori}
+                        <td className="px-12 py-4 whitespace-nowrap">
+                        {item.pakets.kategoriWebsite.nama_kategori}
                         </td>
-                        <td className="mx-auto text-sm py-4 text-center">
-                          Rp {item.harga},00
+                        <td className="px-12 py-4 whitespace-nowrap">
+                        Rp {item.harga},00
                         </td>
-                        <td className="mx-auto text-sm py-4 text-center">
-                          Rp {item.diskon},00
-                        </td>
-                        <td className="mx-auto text-sm py-4 text-center">
-                          Rp {item.pajak},00
-                        </td>
-                        <td className="mx-auto text-sm py-4 text-center">
-                          Rp {item.total},00
+                        <td className="flex items-center gap-1 px-12 py-4 mt-8 whitespace-nowrap">
+                          <Link
+                            href={"/pelanggan/detail_invoice"}
+                          >
+                            <div
+                              className="items-center w-auto px-5 py-2 mb-2 tracking-wider text-white font-semibold rounded-full shadow-sm bg-orange-400 hover:bg-orange-600"
+                              aria-label="edit"
+                            >
+                              Detail
+                            </div>
+                          </Link>
                         </td>
                       </tr>
                     ))}
                   </tbody>
                 </table>
               </div>
-
-              <div className="grid grid-flow-col">
-                <div className="mt-6">
-                  <h3 className="font-bold">Pesan</h3>
-                  <hr className="border-black w-3/4 py-2" />
-                  <textarea
-                    className="w-3/4 border rounded-md p-2"
-                    rows="4"
-                    placeholder="Tulis pesan di sini..."
-                  ></textarea>
-                </div>
-
-                <div className="grid grid-flow-row mt-4">
-                  <div className="flex justify-between text-sm font-semibold mt-2">
-                    <div>Subtotal</div>
-                    <div>Rp {invoiceData.subtotal},00</div>
-                  </div>
-                  <div className="flex justify-between text-sm font-semibold mt-2">
-                    <div>Total Diskon</div>
-                    <div>(Rp {invoiceData.total_diskon},00)</div>
-                  </div>
-                  <div className="flex justify-between text-sm font-semibold mt-2">
-                    <div>PPN</div>
-                    <div>Rp {invoiceData.total_pajak},00</div>
-                  </div>
-                  <div className="flex justify-between text-lg font-bold mt-2 underline">
-                    <div>Total</div>
-                    <div>Rp {invoiceData.total},00</div>
-                  </div>
-                </div>
-              </div>
-
-              <div className="flex justify-end mt-6">
-                <button
-                  onClick={handlePrint}
-                  className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-700 focus:outline-none"
-                >
-                  Cetak Invoice
-                </button>
-              </div>
             </div>
           </div>
         </div>
-      </div>
     </PelangganLayout>
   );
 }
